e2dee7aaf9a2a2da3bbd4a36afa08e448a43f0d2
Angelo <angeloinfante02@gmail.com>
2024-07-09 16:42:37 +0200

    Initial Commit

---
+++ DoggoFinder/DoggoFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoggoFinderApp.swift
+//  DoggoFinder
+//
+//  Created by Studente on 09/07/24.
+//
+
+import SwiftUI
+
+@main
+struct DoggoFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
